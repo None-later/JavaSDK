cd4ddd10b5d7108ee3c48dac6a08011e3c988e0a
egimaben <egimaben@gmail.com>
2016-04-27 23:27:00 +0300

    fallback to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-set -o errexit -o nounset
-
-if [ "$TRAVIS_BRANCH" != "development" ]
-then
-  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
-  exit 0
-fi
-
 rev=$(git rev-parse --short HEAD)
 
 #cd stage/_book
