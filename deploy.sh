107165e5235a3b95c44c478d70793fa70f720dcb
egimaben <egimaben@gmail.com>
2016-04-28 16:41:46 +0300

    used bash command to call deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 rev=$(git rev-parse --short HEAD)
-
-#cd stage/_book
-
-#git init
 git config user.name "egimaben"
 git config user.email "egimaben@gmail.com"
 git config user.password "soulsurvivor1"
